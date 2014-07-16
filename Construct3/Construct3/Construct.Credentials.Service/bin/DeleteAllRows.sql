2fbc7c90130fa65245c8edae4ec7527228fd0e1d
Tyler Camp <tyler.camp@loop.colum.edu>
2014-07-16 13:19:25 -0400

    Changed references to Telerik OpenAccess ORM to be local instead of referring to the locally-installed version (i.e. in Program Files)

--- Construct3/Construct3/Construct.Credentials.Service/bin/DeleteAllRows.sql
+++
@@ -1,8 +0,0 @@
-﻿-- disable all constraints
-EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
-
--- delete data in all tables
-EXEC sp_MSForEachTable "DELETE FROM ?"
-
--- enable all constraints
-exec sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
