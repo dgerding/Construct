e048605c538d3e9d58e7185b15747681e318cbfe
David Gerding <dave@versive.com>
2014-07-17 17:46:10 -0500

    Progress on adding SQL init and schema check/update functionality to Server app.  When complete the server wipe and update function will be removed from the client app.  It's really a server thing.

--- Construct3/Construct3/Construct.Server.Runtime.WindowsUX/MainWindow.xaml.cs
+++ Construct3/Construct3/Construct.Server.Runtime.WindowsUX/MainWindow.xaml.cs
@@ -31,6 +31,13 @@ namespace ConstructServer.Runtime.Windows
 
         private void StartServerButton_Click(object sender, RoutedEventArgs e)
         {
+            Uri serverServiceUri = UriUtility.CreateStandardConstructServiceEndpointUri("http", "Server", "localhost", serverProcessID, publicHostPortBase);
+            int hostPortBase = 8000;
+            string connStringLookupKey = ConnectionStringLookupKey.Text;
+            string connectionString = ConfigurationManager.ConnectionStrings[connStringLookupKey].ConnectionString;
+            EntitiesModel context = new EntitiesModel(connectionString);
+
+                
             if (modelsHost == null)
             {
                 if (Guid.TryParse(ServerProcessIDTextBox.Text, out serverProcessID))
@@ -44,51 +51,63 @@ namespace ConstructServer.Runtime.Windows
                     return;
                 }
 
-                Uri serverServiceUri = UriUtility.CreateStandardConstructServiceEndpointUri("http", "Server", "localhost", serverProcessID, publicHostPortBase);
-                int hostPortBase = 8000;
-                string connStringLookupKey = ConnectionStringLookupKey.Text;
-                string connectionString = ConfigurationManager.ConnectionStrings[connStringLookupKey].ConnectionString;
-                EntitiesModel context = new EntitiesModel(connectionString);
-
-                Server server = new Server(serverServiceUri, hostPortBase, connStringLookupKey);
-                modelsHost = server;
+                //Server server = new Server(serverServiceUri, hostPortBase, connStringLookupKey);
+                modelsHost = new Server(serverServiceUri, hostPortBase, connStringLookupKey);
 
                 modelsHost.Start();
-                IEnumerable<ServiceHost> hostedServices = modelsHost.Hosts;
 
-                foreach (ServiceHost host in hostedServices)
-                {
-                    Services.Add(host);
-                }
-                //if (modelsHost.Start(serverUri, publicHostPortBase))
-                {
-                    StartServerButton.IsEnabled = !StartServerButton.IsEnabled;
-                    StopServerButton.IsEnabled = !StopServerButton.IsEnabled;
+                if (modelsHost.IsDataServiceReady)
+                { 
+                    IEnumerable<ServiceHost> hostedServices = modelsHost.Hosts;
 
-                    ServerProcessIDTextBox.IsEnabled = !ServerProcessIDTextBox.IsEnabled;
-                    PublicHostNameTextBox.IsEnabled = !PublicHostNameTextBox.IsEnabled;
-                    PublicHostPortBaseTextBox.IsEnabled = !PublicHostPortBaseTextBox.IsEnabled;
-                    ConnectionStringLookupKey.IsEnabled = !ConnectionStringLookupKey.IsEnabled;
-                }
-                //else
-                //{
-                //    //logger.
-                //}
-                foreach (DataType dataType in context.DataTypes)
-                {
-                    foreach (PropertyType propertyType in dataType.PropertyParents)
+                    foreach (ServiceHost host in hostedServices)
                     {
-                        ServiceHost host = PropertyServiceManager.StartService(serverServiceUri, dataType, propertyType, connectionString);
                         Services.Add(host);
                     }
+                    //if (modelsHost.Start(serverUri, publicHostPortBase))
+                    {
+                        StartServerButton.IsEnabled = !StartServerButton.IsEnabled;
+                        StopServerButton.IsEnabled = !StopServerButton.IsEnabled;
+
+                        ServerProcessIDTextBox.IsEnabled = !ServerProcessIDTextBox.IsEnabled;
+                        PublicHostNameTextBox.IsEnabled = !PublicHostNameTextBox.IsEnabled;
+                        PublicHostPortBaseTextBox.IsEnabled = !PublicHostPortBaseTextBox.IsEnabled;
+                        ConnectionStringLookupKey.IsEnabled = !ConnectionStringLookupKey.IsEnabled;
+                    }
+                    //else
+                    //{
+                    //    //logger.
+                    //}
+                    foreach (DataType dataType in context.DataTypes)
+                    {
+                        foreach (PropertyType propertyType in dataType.PropertyParents)
+                        {
+                            ServiceHost host = PropertyServiceManager.StartService(serverServiceUri, dataType, propertyType, connectionString);
+                            Services.Add(host);
+                        }
+                    }
+
+                    dbSchemaStatusLabel.Content = modelsHost.IsDatabaseSchemaCurrent.ToString();
+                    dbConnectionStatusLabel.Content = modelsHost.IsDatabaseReachable.ToString();
                 }
-                
+                else
+                {
+                    MessageBox.Show("Construct Server did NOT start.  Check database connection and schema status");
+
+                    dbSchemaStatusLabel.Content = modelsHost.IsDatabaseSchemaCurrent.ToString();
+                    dbConnectionStatusLabel.Content = modelsHost.IsDatabaseReachable.ToString();
+
+                    if(!modelsHost.IsDatabaseSchemaCurrent) 
+                    {
+                        ReinitializeDatabaseButton.Visibility = Visibility.Visible;
+                    }
+
+                }
             }
             else
             {
                 if (modelsHost.IsRunning == true)
                 {
-
                     modelsHost.Stop();
                     StartServerButton.IsEnabled = !StartServerButton.IsEnabled;
                     StopServerButton.IsEnabled = !StopServerButton.IsEnabled;
@@ -98,6 +117,20 @@ namespace ConstructServer.Runtime.Windows
                     PublicHostPortBaseTextBox.IsEnabled = !PublicHostPortBaseTextBox.IsEnabled;
                 }
             }
+
+            if (modelsHost.IsDatabaseSchemaCurrent)
+            {
+               // ADjust label color to call out exceptions
+
+                ReinitializeDatabaseButton.Visibility = Visibility.Hidden;
+                
+            }
+            else
+            {
+
+            }
+
+            
         }
 
         private void StopServerButton_Click(object sender, RoutedEventArgs e)
@@ -141,6 +174,49 @@ namespace ConstructServer.Runtime.Windows
                     throw;
                 }
             }
+        }
+
+        private void ReinitializeDatabaseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Uri serverServiceUri = UriUtility.CreateStandardConstructServiceEndpointUri("http", "Server", "localhost", serverProcessID, publicHostPortBase);
+            int hostPortBase = 8000;
+            string connStringLookupKey = ConnectionStringLookupKey.Text;
+            string connectionString = ConfigurationManager.ConnectionStrings[connStringLookupKey].ConnectionString;
+
+            try
+            {
+                EntitiesModel context = new EntitiesModel(connectionString);
+
+                Telerik.OpenAccess.ISchemaHandler schemaHandler = context.GetSchemaHandler();
+                string script = null;
+                if (schemaHandler.DatabaseExists())
+                {
+                    script = schemaHandler.CreateUpdateDDLScript(null);
+                    
+                }
+                else
+                {
+                    schemaHandler.CreateDatabase();
+                    script = schemaHandler.CreateDDLScript();
+                }
+                if (!string.IsNullOrEmpty(script))
+                {
+                    //context.Connection.Close();
+                    schemaHandler.ExecuteDDLScript(script);
+                }
+
+                MessageBox.Show("Update schema succeded.");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Update schema failed.");
+                throw;
+            }
+            
+
+            //get contenxt and do that thing
         }
     }
 }
