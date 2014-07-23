c21afecdf50c3f6de191278685808876bc7578c6
Tyler Camp <tyler.camp@loop.colum.edu>
2014-07-22 20:12:28 -0400

    (Include actual utterance datatype) (Include helper class for previous WCF bandwidth expansion)

---
+++ Construct3/Construct3/Construct.UX.ViewModels/ModelClientHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace Construct.UX.ViewModels
+{
+	public static class ModelClientHelper
+	{
+		public static void EnhanceModelClientBandwidth<T>(T modelClient)
+		{
+			Type clientType = typeof(T);
+			var endpointPropertyInfo = clientType.GetProperty("Endpoint");
+			ServiceEndpoint clientEndpoint = endpointPropertyInfo.GetValue(modelClient, null) as ServiceEndpoint;
+
+			if (clientEndpoint == null)
+				return;
+
+			WSDualHttpBinding clientBinding = clientEndpoint.Binding as WSDualHttpBinding;
+			if (clientBinding == null)
+				return;
+
+			clientBinding.MaxBufferPoolSize = 2147483647;
+			clientBinding.MaxReceivedMessageSize = 2147483647;
+			clientBinding.ReaderQuotas.MaxArrayLength = 2147483647;
+			clientBinding.ReaderQuotas.MaxBytesPerRead = 2147483647;
+			clientBinding.ReaderQuotas.MaxDepth = 2147483647;
+			clientBinding.ReaderQuotas.MaxStringContentLength = 2147483647;
+			clientBinding.ReaderQuotas.MaxNameTableCharCount = 2147483647;
+		}
+	}
+}
---
+++ ConstructSensors/WindowsSensors/Utterance/Utterance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utterance
+{
+	public class Utterance
+	{
+		public Utterance()
+		{
+
+		}
+
+		public Utterance(byte[] wavFileData)
+		{
+			FullWAVFileData = wavFileData;
+			UtteranceEndTime = DateTime.UtcNow;
+		}
+
+		public byte[] FullWAVFileData;
+		public DateTime UtteranceEndTime;
+	}
+}
